4b0a281651311c04fcd7c0c2e7671a882099659c
Bin Yan <byan@Bin-Yans-iMac.local>
2017-06-27 19:45:12 -0400

    update

---
+++ images/js/custom.js
@@ -0,0 +1,38 @@
+
+
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
+
+  $(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
+
+
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+
+
+  /* Back top
+  -----------------------------------------------*/
+  
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
